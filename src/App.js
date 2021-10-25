592023492705df2e65276a3c885287ae7bf0d616
jeshurun ezeobi <jeshurunezeobi@jeshuruns-MacBook-Pro.local>
2021-10-25 15:46:21 -0700

    refactor and create Input component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,15 +6,23 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(()=>{
+    const storedLoggedInUserInfo = localStorage.getItem("is-logged-in");
+    if(storedLoggedInUserInfo === "1"){
+      setIsLoggedIn(true);
+    }
+  },[]);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("is-logged-in","1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem("is-logged-in");
   };
 
   return (
